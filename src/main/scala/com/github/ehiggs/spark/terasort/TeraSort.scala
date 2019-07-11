6661fb02fca226cd8c7d98ef54b6e1450de93d3f
Ruslan Scherbakov rscherba@azul.com <rscherba@azul.com>
2019-07-11 09:27:13 -0700

    Terasort with iterations

--- src/main/scala/com/github/ehiggs/spark/terasort/TeraSort.scala
+++ src/main/scala/com/github/ehiggs/spark/terasort/TeraSort.scala
@@ -44,7 +44,7 @@ object TeraSort {
       println("DRIVER_MEMORY=50g spark-submit " +
         "com.github.ehiggs.spark.terasort.TeraSort " +
         "spark-terasort-1.0-SNAPSHOT-with-dependencies.jar " +
-        "/home/myuser/terasort_in /home/myuser/terasort_out")
+        "/home/myuser/terasort_in /home/myuser/terasort_out num_iterations")
       System.exit(0)
     }
 
@@ -52,16 +52,32 @@ object TeraSort {
     val inputFile = args(0)
     val outputFile = args(1)
 
+    var numIterations = 1;
+    if (args.length > 2) {
+      numIterations = args(2).toInt;
+    }
+
     val conf = new SparkConf()
       .set("spark.serializer", "org.apache.spark.serializer.KryoSerializer")
       .setAppName(s"TeraSort")
+
     val sc = new SparkContext(conf)
 
-    val dataset = sc.newAPIHadoopFile[Array[Byte], Array[Byte], TeraInputFormat](inputFile)
-    val sorted = dataset.repartitionAndSortWithinPartitions(
-      new TeraSortPartitioner(dataset.partitions.length))
-    sorted.saveAsNewAPIHadoopFile[TeraOutputFormat](outputFile)
-    
+    for (iter <- 1 until (numIterations + 1) ) {
+
+      val start = System.currentTimeMillis()
+      println("STARTED iteration: " + iter + ". " + start + " ;" +
+              java.time.LocalDateTime.now)
+      val dataset = sc.newAPIHadoopFile[Array[Byte], Array[Byte], TeraInputFormat](inputFile)
+      val sorted = dataset.repartitionAndSortWithinPartitions(
+        new TeraSortPartitioner(dataset.partitions.length))
+      sorted.saveAsNewAPIHadoopFile[TeraOutputFormat](outputFile)
+      val end = System.currentTimeMillis()
+      println("DONE iteration: " + iter +
+              ". Took " + (end - start) + "ms; " +
+              end + " ;" + java.time.LocalDateTime.now)
+    }
+
     sc.stop()
   }
 }
